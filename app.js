6ed028531acdae9acc0f5bb65add2acaa3bf7d35
Austin Adams <austin@farmingtonwebdesign.net>
2015-07-06 14:40:21 -0700

    init v1

---
+++ app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by austinadams on 7/6/15.
+ */
+angular.module('tableMulti',[])
+  .controller('MultiplicationCtrl', function($scope){
+    $scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  });
