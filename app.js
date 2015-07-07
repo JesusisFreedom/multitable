975ad5e12b5e7dcc054582dc75b3c86e907e4884
Austin Adams <austin@farmingtonwebdesign.net>
2015-07-06 17:58:36 -0700

    Follow Training

--- app.js
+++ app.js
@@ -2,6 +2,18 @@
  * Created by austinadams on 7/6/15.
  */
 angular.module('tableMulti',[])
-  .controller('MultiplicationCtrl', function($scope){
-    $scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  .controller('MultiplicationCtrl', function($scope, $attrs){
+    $scope.initialNumberLimit  = $attrs.limit || 10;
+
+    $scope.numbers = function(){
+      var num = [];
+      for(var i = 0; i < $scope.initialNumberLimit; i++){
+        num[i] = i + 1;
+      }
+      return num;
+    };
+
+    $scope.compute = function(a,b){
+      return a * b;
+    };
   });
